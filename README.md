2f0f7ac40f650b1bc38bca582f2f68296f76a5ea
Wilbur Wei <wilburw2026@gmail.com>
2026-03-21 14:03:58 -0400

    Implement some base UI and user flow information.

--- README.md
+++ README.md
@@ -3,3 +3,42 @@
 ## Goals
  - Implement Airplane Chess
  - Multiplayer functionality?
+
+## TODO
+ - [x] Figure out how to zoom and scroll animations
+ - [ ] Figure out how Pixi.js works 
+ - [ ] Use Socket.io for multiplayer functions?
+ - [ ] Create game UI
+ - [ ] In game chat?
+ - [x] Figure out theme
+   - [x] Minimliast game theme? 
+
+## Notes
+
+Theme colors:
+ - White and light gray
+ - Muted colors
+   - Red: Soft Terracotta (#EE6C4D)
+   - Blue: Dusty Sky (#4e7dba)
+   - Green: Sage Leaf (#8FB9A8)
+   - Yellow: Muted Mustard (#F4D35E)
+
+Game FLow
+ - Presses start button
+   - Shows game screen
+   - Waits for players
+   - Show start game button if there is 2-4 people in the game
+   - Play game
+     - End game if any users disconnect
+   - View game if you won already
+   - Show results and option to return to home screen
+ - Presses join game
+   - Show game screen
+   - Show number of players 
+   - Waits for the creator of the host game to start game
+   - Play game
+   - Viewer mode when you won already
+   - Ends game if users disconnect
+   - Show results and option to return to home screen
+ - For both of these, if the host leaves the waiting room for a game
+   - Show message and return to home screen
--- src/main.js
+++ src/main.js
@@ -1,35 +1,38 @@
-import { Application, Assets, Sprite } from "pixi.js";
+import { Application, Assets, Sprite, Texture } from "pixi.js";
+import { Viewport } from "pixi-viewport";
 
+
+$("#start").on("click", function () {
+  console.log("do something here")
+});
+
+$("#join").on("click", function() {
+  console.log("do something here")
+})
 (async () => {
   // Create a new application
   const app = new Application();
 
   // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
+  await app.init({ background: "#dce4f5", resizeTo: window });
 
   // Append the application canvas to the document body
   document.getElementById("pixi-container").appendChild(app.canvas);
 
-  // Load the bunny texture
-  const texture = await Assets.load("/assets/bunny.png");
-
-  // Create a bunny Sprite
-  const bunny = new Sprite(texture);
-
-  // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
-
-  // Move the sprite to the center of the screen
-  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
-
-  // Add the bunny to the stage
-  app.stage.addChild(bunny);
-
-  // Listen for animate update
-  app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
+  // create viewport
+  const viewport = new Viewport({
+    screenWidth: window.innerWidth,
+    screenHeight: window.innerHeight,
+    worldWidth: 1000,
+    worldHeight: 1000,
+    events: app.renderer.events, // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
   });
+
+  // add the viewport to the stage
+  app.stage.addChild(viewport);
+
+  // activate plugins
+  viewport.drag().pinch().wheel().decelerate();
+
+  
 })();
